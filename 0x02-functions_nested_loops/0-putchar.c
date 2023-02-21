b629a5b9bd1c4041c219ce0299c7bacde709c160
salemgathura <salemgathura72@gmail.com>
2023-02-21 12:09:49 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * This program prints the characters in the word "_putchar"
+ * using the _putchar function.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+
+    return (0);
+}

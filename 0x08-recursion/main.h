43d9a3a1992c4f7eaf57633eb38a39fde87b5fa3
Salem Gathura <salemgathura72@gmail.com>
2023-09-27 11:53:21 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function that prints a string followed by a newline */
+void _puts_recursion(char *s);
+
+/* Function that writes a character to stdout */
+int _putchar(char c);
+
+/* Function that prints a string in reverse */
+void _print_rev_recursion(char *s);
+
+/* Function that returns the length of a string */
+int _strlen_recursion(char *s);
+
+/* Function that returns the factorial of a given number */
+int factorial(int n);
+
+/* Function that returns the value of x raised to the power of y */
+int _pow_recursion(int x, int y);
+
+/* Function that returns the natural square root of a number */
+int _sqrt_recursion(int n);
+
+/* Function that checks if a number is prime */
+int is_prime_number(int n);
+
+/* Function that checks if a string is a palindrome */
+int is_palindrome(char *s);
+
+#endif
